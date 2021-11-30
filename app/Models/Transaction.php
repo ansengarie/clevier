88cc552a2d23ae8fcafba991f8358102ad9ac0fd
ajins15 <ansengarie@gmail.com>
2021-11-30 18:15:24 +0700

    make model app

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'users_id', 'name', 'email', 'address', 'phone', 'courier',
+        'payment', 'payment_url', 'total_price', 'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
