5271b384727c3efd8e51ba94d06dbf085e9bf89a
ajins15 <ansengarie@gmail.com>
2021-12-02 15:32:09 +0700

    create frontend controller

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -13,16 +13,19 @@
 
     {{-- My Style --}}
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/content.css">
+    <link rel="stylesheet" href="/css/header.css">
+
     <title>Innerior | {{ $title }}</title>
   </head>
   <body>
-    @include('partials.navbar')
+    @include('components.frontend.navbar')
 
-    <div class="container mt-3">
+    <div class="container mt-2">
       @yield('container')
     </div>
 
-    @include('partials.footer')
+    @include('components.frontend.footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer ></script> 
   </body>
