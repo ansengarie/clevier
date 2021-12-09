61f9ae7cbec4c847986b996d99716efcf2f4f830
ajins15 <ansengarie@gmail.com>
2021-12-10 01:36:20 +0700

    seeder user, category, products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Bezhanov\Faker\Provider\Commerce;
 
 class ProductFactory extends Factory
 {
@@ -11,10 +13,17 @@ class ProductFactory extends Factory
      *
      * @return array
      */
+
+    protected $model = Product::class;
+
     public function definition()
     {
         return [
-            //
+            'category_id' => $this->faker->numberBetween(1, 5),
+            'name' => $this->faker->name(),
+            'price' => $this->faker->numberBetween(100000, 2000000),
+            'description' => $this->faker->paragraph(2),
+            'slug' => $this->faker->slug()
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use \Bezhanov\Faker\ProviderCollectionHelper;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Aji Nuansa Sengarie',
+            'username' => 'ansengarie',
+            'email' => 'ansengarie@gmail.com',
+            'password' => bcrypt('sengarie9')
+        ]);
+
+        \App\Models\User::factory(10)->create();
+
+        Category::create([
+            'name' => 'Living Room',
+            'slug' => 'living-room'
+        ]);
+
+        Category::create([
+            'name' => 'Kitchen',
+            'slug' => 'kitchen'
+        ]);
+
+        Category::create([
+            'name' => 'Bathroom',
+            'slug' => 'bathroom'
+        ]);
+
+        Category::create([
+            'name' => 'Bedroom',
+            'slug' => 'bedroom'
+        ]);
+
+        Category::create([
+            'name' => 'Family Room',
+            'slug' => 'family-room'
+        ]);
+
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+        Product::factory(10)->create();
     }
 }
